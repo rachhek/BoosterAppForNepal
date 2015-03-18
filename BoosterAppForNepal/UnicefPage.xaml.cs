3e4f1bb9c33432ab2b9e2ab5b67b3095fbf94f7d
rachhek <rachhek@gmail.com>
2015-03-19 00:29:45 +0545

    Khop for Hope Initial version

---
+++ BoosterAppForNepal/UnicefPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace BoosterAppForNepal
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class UnicefPage : Page
+    {
+        public UnicefPage()
+        {
+            this.InitializeComponent();
+            events.DataContext = newEvent;
+            
+            newEvent.Add(new newEvents("IPV", "Polio", new DateTime(2014,06,16),"Itahari","New immunization camp"));
+            newEvent.Add(new newEvents("IPV", "Polio", new DateTime(2014, 06, 16), "Itahari", "New immunization camp"));
+            newEvent.Add(new newEvents("IPV", "Polio", new DateTime(2014, 06, 16), "Itahari", "New immunization camp"));
+            newEvent.Add(new newEvents("IPV", "Polio", new DateTime(2014, 06, 16), "Itahari", "New immunization camp"));
+        }
+
+
+        public class newEvents
+        {
+            public newEvents() { }
+
+            public newEvents(string vaccine, string disease, DateTime date, string location, string description)
+            {
+                Vaccine = vaccine;
+                Disease = disease;
+                Date = date;
+                Location = location;
+                Description = description;
+            }
+            public string Vaccine { get; set; }
+            public string Disease { get; set; }
+            public DateTime Date { get; set; }
+            public string Location { get; set; }
+            public string Description {get; set;}
+            
+
+            public override string ToString()
+            {
+                return Vaccine + " on " + Date.ToString("d") + " at " + Location;
+            }
+        }
+        public ObservableCollection<newEvents> newEvent = new ObservableCollection<newEvents>();
+
+        private void addEvent_click(object sender, RoutedEventArgs e)
+        {
+            newEvent.Add(new newEvents(vaccineName.Text, diseaseName.Text, new DateTime(2014, 06, 16), location.Text, description.Text));
+        }
+
+        private void backButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.GoBack();
+        }
+    }
+}
---
+++ BoosterAppForNepal/users.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Notifications;
+using Windows.UI.Popups;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace BoosterAppForNepal
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class users : Page
+    {
+        public users()
+        {
+            this.InitializeComponent();
+        }
+        private void upcoming_button_click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Upc));
+        }
+
+        private void backButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.GoBack();
+        }
+
+        private void reminder_click(object sender, RoutedEventArgs e)
+        {
+            if (this.BottomAppBar != null)
+            {
+                this.BottomAppBar.IsOpen = true;
+            }
+        }
+
+        private void eventDetail_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(EventDetail));
+        }
+
+      
+        private async void OnReminderButtonClicked(object sender, RoutedEventArgs e)
+        {
+            var notifier = ToastNotificationManager.CreateToastNotifier();
+
+            // Make sure notifications are enabled
+            if (notifier.Setting != NotificationSetting.Enabled)
+            {
+                var dialog = new MessageDialog("Notifications are currently disabled");
+                await dialog.ShowAsync();
+                return;
+            }
+
+            // Get a toast template and insert a text node containing a message
+            var template = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastText01);
+            var element = template.GetElementsByTagName("text")[0];
+            element.AppendChild(template.CreateTextNode("Heads Up !! Tommorow !!"));
+            element.AppendChild(template.CreateTextNode("Anish's 4 of 5 DTap Vaccine. !!"));
+
+            // Schedule the toast to appear 30 seconds from now
+            var date = DateTimeOffset.Now.AddSeconds(10);
+            var stn = new ScheduledToastNotification(template, date);
+            notifier.AddToSchedule(stn);
+        }
+    }
+    
+}
